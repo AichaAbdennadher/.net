2aaa60afcdd7680c4dc642ebd23f16050f1284ed
aicha <abdennadheraicha19@gmail.com>
2025-12-14 14:30:47 +0100

    CreateOrd

--- front/wwwroot/js/notifications.js
+++
@@ -1,9 +0,0 @@
-﻿window.showNotification = (message, type) => {
-    // type = "success", "error", "warning"
-    const bg = type === "success" ? "green" : type === "error" ? "red" : "yellow";
-    const toast = document.createElement("div");
-    toast.textContent = message;
-    toast.className = `fixed top-5 right-5 bg-${bg}-500 text-white px-4 py-2 rounded shadow-lg`;
-    document.body.appendChild(toast);
-    setTimeout(() => { toast.remove(); }, 4000);
-};
